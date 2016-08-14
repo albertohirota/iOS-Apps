b7d43288a5a0987a5a5cea527250126b6fe48bcb
Alberto Hirota <alberto.hirota@gmail.com>
2016-08-14 14:10:07 -0400

    App Movie - Start Project

---
+++ MovieApp2/MovieApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieApp2
+//
+//  Created by Alberto Hirota on 8/14/16.
+//  Copyright © 2016 Alberto Hirota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
